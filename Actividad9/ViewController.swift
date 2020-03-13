24785dae5d07ec336bbcfcbfd07592156dcb6ad9
David Salazar <davidsalazar@2806-108e-0018-3097-8121-30ca-243b-8952.ipv6.infinitum.net.mx>
2020-03-12 22:52:22 -0600

    Initial Commit

---
+++ Actividad9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actividad9
+//
+//  Created by David Salazar on 12/03/20.
+//  Copyright © 2020 David Salazar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
